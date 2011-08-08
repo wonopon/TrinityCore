75fc28a865deca61b80a9fff6c7ff54cc0e9f2ec
wonopon <wonopon12@aol.com>
2011-08-08 17:45:27 -0300

    Fix Northrend rare elite faction issues

--- sql/updates/world/2011_05_10_00_world_creature.sql
+++ sql/updates/world/2011_05_10_00_world_creature.sql
@@ -4,3 +4,25 @@ UPDATE `creature_model_info` SET `bounding_radius`=2,`combat_reach`=4.6,`gender`
 DELETE FROM `creature_template_addon` WHERE `entry`=28221;
 INSERT INTO `creature_template_addon` (`entry`,`mount`,`bytes1`,`bytes2`,`emote`,`auras`) VALUES
 (28221,0,0,1,0, '11959');
+
+--Northrend Rare Elite Fixes
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32398; --king ping
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32398;
+ 
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32377; --perobas the bloodthirster
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32377;
+ 
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32400; --tukemuth
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32400;
+ 
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32438; --Syreian the Bonecarver
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32438;
+ 
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32475; --Terror Spinner
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32475;
+ 
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32481; --Aotona
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32481;
+ 
+UPDATE creature_template SET faction_A = 14 WHERE entry = 32495; --Hildana Deathstealer
+UPDATE creature_template SET faction_H = 14 WHERE entry = 32495;
